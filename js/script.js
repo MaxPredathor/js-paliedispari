3adf9de9ba6620debe62b3d46346bdc9c02ca7a4
MaxPredathor <max.gilli98@gmail.com>
2023-10-17 15:51:56 +0200

    added printer function

--- js/script.js
+++ js/script.js
@@ -1,46 +1,56 @@
-const btn = document.getElementById('button-1');
-const checkBtn = document.getElementById('button-2');
-const inputEl2 = document.getElementById('data-2').value;
-const resultEl = document.querySelector('.alert');
-const resultEl2 = document.querySelector('.alert-2');
+    const btn = document.getElementById('button-1');
+    const checkBtn = document.getElementById('button-2');
+    const inputEl2 = document.getElementById('data-2').value;
+    const resultEl2 = document.querySelector('.alert-2');
 
+    btn.addEventListener('click', function(){
 
-
-
-btn.addEventListener('click', function(){
-
-    const inputEl = document.getElementById('data').value;
-    palindromo(inputEl);  
-  
-});
-
-function palindromo(input){
-    const splitWord = [];
-    const reverseWord = [];
-
-    for(let x = 0; x < input.length; x++){
-        console.log(input[x]);
-        splitWord.push(input[x]);
-
-    }
-    console.log(splitWord);
+        const inputEl = document.getElementById('data').value;
+        palindromo(inputEl);  
+      
+    });
     
-
-    for(let x = input.length - 1; x >= 0; x--){
-        console.log(input[x]);
-        reverseWord.push(input[x]);
-    }
-    console.log(reverseWord);
+    function palindromo(input){
+        const splitWord = [];
+        const reverseWord = [];
     
-    let stringSplit = splitWord.join("");
-    let stringaReverse = reverseWord.join("");
-
-    if(stringSplit === stringaReverse){
-        console.log('La tua parola è un Palindromo');
-    } else{
-        console.log('La tua parola NON è un Palindromo');
+        for(let x = 0; x < input.length; x++){
+            console.log(input[x]);
+            splitWord.push(input[x]);
+    
+        }
+        console.log(splitWord);
+        
+    
+        for(let x = input.length - 1; x >= 0; x--){
+            console.log(input[x]);
+            reverseWord.push(input[x]);
+        }
+        console.log(reverseWord);
+        
+        let stringSplit = splitWord.join("");
+        let stringaReverse = reverseWord.join("");
+    
+        if(stringSplit === stringaReverse){
+            printer('alert-success', 'La tua parola è un Palindromo')
+            console.log('La tua parola è un Palindromo');
+        } else{
+            printer('alert-danger', 'La tua parola NON è un Palindromo')
+            console.log('La tua parola NON è un Palindromo');
+        }
     }
-}
+
+    function printer(alertColor, msg){
+
+        const resultEl = document.querySelector('.alert');
+        resultEl.classList.add(alertColor);
+        resultEl.classList.remove('d-none')
+        resultEl.innerText = msg;
+    }
+
+
+
+
 
 
 
--- js/utilities.js
+++ js/utilities.js
@@ -2,3 +2,33 @@
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
   }
+
+
+
+function palindromo(input){
+    const splitWord = [];
+    const reverseWord = [];
+
+    for(let x = 0; x < input.length; x++){
+        console.log(input[x]);
+        splitWord.push(input[x]);
+
+    }
+    console.log(splitWord);
+    
+
+    for(let x = input.length - 1; x >= 0; x--){
+        console.log(input[x]);
+        reverseWord.push(input[x]);
+    }
+    console.log(reverseWord);
+    
+    let stringSplit = splitWord.join("");
+    let stringaReverse = reverseWord.join("");
+
+    if(stringSplit === stringaReverse){
+        console.log('La tua parola è un Palindromo');
+    } else{
+        console.log('La tua parola NON è un Palindromo');
+    }
+}
