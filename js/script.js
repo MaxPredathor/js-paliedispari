ef26155958871d851b35d6a2efdbb0ed9452ae6f
MaxPredathor <max.gilli98@gmail.com>
2023-10-17 14:49:21 +0200

    repo creation

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const btn = document.getElementById('button-1');
+const checkBtn = document.getElementById('button-2');
+const inputEl2 = document.getElementById('data-2').value;
+const resultEl = document.querySelector('.alert');
+const resultEl2 = document.querySelector('.alert-2');
+
+
+
+
+
+
+
+
+// checkBtn.addEventListener('click', function(){
+    
+// });
+
+
+
+
+// function getRndInteger(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1) ) + min; 
+//   }
+
+
+// function reverseInput(stringa){
+    
+    
+// }
+
+// function reverseString(str) {
+//     let newString = "";
+//     for (let i = str.length - 1; i >= 0; i--) {
+//         newString += str[i];
+//         splitWord.push(newString)
+//     }
+//     return newString;
+// } 
+// reverseString('new');
